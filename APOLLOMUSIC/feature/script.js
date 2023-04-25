c15f0b79372c1cd0d65a8bb8d7c7f5b3c595a73b
siddiquiadab <80346223+adab786@users.noreply.github.com>
2023-04-25 21:21:42 +0530

    file added

---
+++ APOLLOMUSIC/feature/script.js
@@ -0,0 +1,256 @@
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio("songs/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let masterSongName = document.getElementById("masterSongName");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
+
+let songs = [
+  {
+    songName: "Warriyo - Mortals [NCS Release]",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "Cielo - Huma-Huma",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
+  },
+  {
+    songName: "DEAF KEV - Invincible [NCS Release]-320k",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
+  },
+  {
+    songName: "Different Heaven & EH!DE - My Heart [NCS Release]",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jpg",
+  },
+  {
+    songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/5.jpg",
+  },
+  {
+    songName: "Rabba - Salam-e-Ishq",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/6.jpg",
+  },
+  {
+    songName: "Sakhiyaan - Salam-e-Ishq",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/7.jpg",
+  },
+  {
+    songName: "Bhula Dena - Salam-e-Ishq",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/8.jpg",
+  },
+  {
+    songName: "Tumhari Kasam - Salam-e-Ishq",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/9.jpg",
+  },
+  {
+    songName: "Na Jaana - Salam-e-Ishq",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/10.jpg",
+  },
+];
+
+songItems.forEach((element, i) => {
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
+
+// Handle play/pause click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
+});
+// Listen to Events
+audioElement.addEventListener("timeupdate", () => {
+  // Update Seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
+
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+    (element) => {
+      element.classList.remove("fa-pause-circle");
+      element.classList.add("fa-play-circle");
+    }
+  );
+};
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      makeAllPlays();
+      songIndex = parseInt(e.target.id);
+      e.target.classList.remove("fa-play-circle");
+      e.target.classList.add("fa-pause-circle");
+      audioElement.src = `songs/${songIndex + 1}.mp3`;
+      masterSongName.innerText = songs[songIndex].songName;
+      audioElement.currentTime = 0;
+      audioElement.play();
+      gif.style.opacity = 1;
+      masterPlay.classList.remove("fa-play-circle");
+      masterPlay.classList.add("fa-pause-circle");
+    });
+  }
+);
+
+document.getElementById("next").addEventListener("click", () => {
+  if (songIndex >= 9) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
+
+document.getElementById("previous").addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 0;
+  } else {
+    songIndex -= 1;
+  }
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
+
+// JavaScript code
+// function search_animal() {
+//   let input = document.getElementById("searchbar").value;
+//   input = input.toLowerCase();
+//   let x = document.getElementsByClassName("songname");
+
+//   for (i = 0; i < x.length; i++) {
+//     if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//       x[i].style.display = "none";
+//     } else {
+//       x[i].style.display = "list-item";
+//     }
+//   }
+// }
+
+let suggestions = [
+  "Warriyo - Mortals [NCS Release]",
+  "Cielo - Huma-Huma",
+  "DEAF KEV - Invincible [NCS Release]-320k",
+  "Janji-Heroes-Tonight-feat-Johnning-NCS-Release",
+  "Channel",
+  "Tumhari Kasam - Salam-e-Ishq",
+  "CodingLab",
+  "Bhula Dena - Salam-e-Ishq",
+  "YouTube",
+  "YouTuber",
+  "YouTube Channel",
+  "Blogger",
+  "Bollywood",
+  "Vlogger",
+  "Vechiles",
+  "Facebook",
+  "Freelancer",
+  "Facebook Page",
+  "Designer",
+  "Developer",
+  "Web Designer",
+  "Web Developer",
+  "Login Form in HTML & CSS",
+  "How to learn HTML & CSS",
+  "How to learn JavaScript",
+  "How to became Freelancer",
+  "How to became Web Designer",
+  "How to start Gaming Channel",
+  "How to start YouTube Channel",
+  "What does HTML stands for?",
+  "What does CSS stands for?",
+];
+
+// getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+
+// if user press any key and release
+inputBox.onkeyup = (e) => {
+  let userData = e.target.value; //user enetered data
+  let emptyArray = [];
+  if (userData) {
+    icon.onclick = () => {
+      webLink = `https://www.google.com/search?q=${userData}`;
+      linkTag.setAttribute("href", webLink);
+      linkTag.click();
+    };
+    emptyArray = suggestions.filter((data) => {
+      //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+    });
+    emptyArray = emptyArray.map((data) => {
+      // passing return data inside li tag
+      return (data = `<li>${data}</li>`);
+    });
+    searchWrapper.classList.add("active"); //show autocomplete box
+    showSuggestions(emptyArray);
+    let allList = suggBox.querySelectorAll("li");
+    for (let i = 0; i < allList.length; i++) {
+      //adding onclick attribute in all li tag
+      allList[i].setAttribute("onclick", "select(this)");
+    }
+  } else {
+    searchWrapper.classList.remove("active"); //hide autocomplete box
+  }
+};
+
+function select(element) {
+  let selectData = element.textContent;
+  inputBox.value = selectData;
+  icon.onclick = () => {
+    webLink = `https://www.google.com/search?q=${selectData}`;
+    linkTag.setAttribute("href", webLink);
+    linkTag.click();
+  };
+  searchWrapper.classList.remove("active");
+}
+
+function showSuggestions(list) {
+  let listData;
+  if (!list.length) {
+    userValue = inputBox.value;
+    listData = `<li>${userValue}</li>`;
+  } else {
+    listData = list.join("");
+  }
+  suggBox.innerHTML = listData;
+}
---
+++ APOLLOMUSIC/javascript.js
@@ -0,0 +1,4 @@
+TweenMax.from(".signup-form", 2, { right: 15 });
+var t = gsap.timeline();
+t.to("#def", { duration: 5, y: -200 });
+// t.from(".wholepage", { duration: 1, y: 10, x: 10, z: 10, repeat: -1 });
